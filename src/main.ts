76565d9faa913da4964e0fc64ce9e8431aa9c1f3
정보경 <bokyung.jung@medistream.co.kr>
2022-12-18 21:40:21 +0900

    CORS 세팅 변경

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('API For Bootcamp')
